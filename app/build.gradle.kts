1e7bc56e3db5132b80623c04ddad6b5500912240
Webster-02 <malikawais2200427@gmail.com>
2026-09-13 16:43:24 -0700

    Add AndroidX dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,11 +15,12 @@ android {
         versionName = "1.0"
     }
 
-    buildFeatures {
-        viewBinding = true
-    }
+    buildFeatures { viewBinding = true }
 }
 
-kotlin {
-    jvmToolchain(17)
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
+
+kotlin { jvmToolchain(17) }
