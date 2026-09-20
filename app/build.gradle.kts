663aa2fa33d0f73f290bbf18bb485bea8404069a
Webster-02 <malikawais2200427@gmail.com>
2026-09-19 17:20:12 -0700

    Bump app version for stability release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.superiorstudent.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildFeatures { viewBinding = true }
